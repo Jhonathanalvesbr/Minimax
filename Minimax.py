bed60385a070ac4666d9dc62c8bd5f6d8fee816f
Jhonathan Alves <Jhonathanalves_br@hotmail.com>
2021-10-08 08:50:49 -0300

    update

--- Minimax.py
+++ Minimax.py
@@ -13,45 +13,34 @@ for i in range(3):
     aux.id = i
     player.append(aux)
 
-def copy(player):
 
-    auxPlayer = []
-    for p in player:
-        aux = Personagem()
-        aux.vida = p.vida
-        auxPlayer.append(aux)
-    
-    return auxPlayer
-
-def minimax(player, playerInimigo, jogada):
-    if(len(player) == 0):
-        return 1
-    if(len(playerInimigo) == 0):
-        return -1
-    
-    player = copy(player)
-    playerInimigo = copy(playerInimigo)
+def minimax(no, jogada):
+    if(len(no.player) == 0):
+        no.heuristica = no.parente.altura + 1
+        return 
+    if(len(no.playerInimigo) == 0):
+        no.heuristica = no.parente.altura + 1
+        return 
     
     if(jogada):
-        for p in player:
-            for i in playerInimigo:
-                if(i.vida <= 0):
-                    playerInimigo.remove(i)
-                i.vida -= p.ataque
-                return minimax(player, playerInimigo, True)
-                i.vida += p.ataque
+        for p in no.player:
+            for i in no.playerInimigo:
+                aux = No()
+                aux.altura = no.altura + 1
+                aux.parente = no
     else:
-         for i in playerInimigo:
-            for p in player:
-                if(p.vida <= 0):
-                    player.remove(p)
-                p.vida -= i.ataque
-                return minimax(player, playerInimigo, False)
-                p.vida += i.ataque
+         for i in no.playerInimigo:
+            for p in no.player:
+                aux = No()
+                aux.altura = no.altura + 1
+                aux.parente = no
 
     jogada = not jogada
-    print(t)
 
 
-print(minimax(player, playerInimigo, True))
+no = No()
+no.altura = 0
+no.player = player
+no.playerInimigo = playerInimigo
 
+print(minimax(no, True))
--- No.py
+++ No.py
@@ -1,14 +1,26 @@
 from math import inf
+from Personagem import Personagem
+
+def copy(player):
+
+    auxPlayer = []
+    for p in player:
+        aux = Personagem()
+        aux.vida = p.vida
+        auxPlayer.append(aux)
+    
+    return auxPlayer
 
 class No:
-    familia = None
+    parente = None
     altura = None
     alpah = inf
     beta = -inf
+    heuristica = None
 
     def __init__(self, player, playerInimigo):
-        self.player = player
-        self.playerInimigo = playerInimigo
+        self.player = copy(player)
+        self.playerInimigo = copy(playerInimigo)
         self.id = []
         self.id.append(player.id)
         self.id.append(playerInimigo.id)
