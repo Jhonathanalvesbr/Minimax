ce33a60475e7a5603acd9c030e6f44129a9d058c
Jhonathan Alves <jhonathanalves_br@hotmail.com>
2021-10-12 14:53:14 -0300

    update

--- Minimax.java
+++ Minimax.java
@@ -8,8 +8,6 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
