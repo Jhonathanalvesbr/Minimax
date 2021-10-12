817ee98db8cdd0857ecdacd0f8421980a57f706d
Jhonathan <Jhonathanalves_br@hotmail.com>
2021-10-12 20:03:53 -0300

    update

--- Minimax.java
+++ Minimax.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import javax.swing.JPanel;
 import java.awt.Graphics2D;
@@ -5,11 +6,15 @@ import java.awt.Graphics;
 import java.awt.event.MouseListener;
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import javax.swing.UIManager;
 
 public class Minimax {
 
@@ -24,13 +29,15 @@ public class Minimax {
     }
 
     public static class Personagem implements Cloneable {
+
         public ArrayList<PlayerImagem> playerImagem;
         int vida = 2;
+        JProgressBar life = new JProgressBar(0,vida);
         int ataque = 1;
         int level = 1;
         int id = 0;
         int indice;
-        int time = 50;
+        int time = 150;
         int tempo = 0;
         int x = 0;
         int y = 0;
@@ -40,10 +47,10 @@ public class Minimax {
             return (Personagem) super.clone();
         }
 
-        public void update(){
-            if(tempo > time){
+        public void update() {
+            if (tempo > time) {
                 indice++;
-                if(indice > playerImagem.size()-1){
+                if (indice > playerImagem.size() - 1) {
                     indice = 0;
                 }
                 playerImagem.get(indice).x = x;
@@ -51,66 +58,162 @@ public class Minimax {
                 tempo = 0;
             }
             tempo++;
+            life.setValue(vida);
         }
-    }  
-    
-    public static class PlayerImagem{
+    }
+
+    public static class PlayerImagem {
+
         BufferedImage imagem;
         int x;
         int y;
-        public PlayerImagem(BufferedImage imagem, int x, int y){
+
+        public PlayerImagem(BufferedImage imagem, int x, int y) {
             this.imagem = imagem;
             this.x = x;
             this.y = y;
         }
     }
 
+    public static ArrayList<PlayerImagem> virar(ArrayList<PlayerImagem> imagem) {
+        for (int i = 0; i < imagem.size(); i++) {
+            imagem.get(i).imagem = Minimax.flip(imagem.get(i).imagem);
+        }
+        return imagem;
+    }
+
+    public static BufferedImage flip(BufferedImage image) {
+        AffineTransform tx = tx = AffineTransform.getScaleInstance(-1, 1);
+        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+        tx.translate(-image.getWidth(null), 0);
+        op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+        image = op.filter(image, null);
+
+        return image;
+    }
+
     public static class TelaGame extends JPanel implements MouseListener {
         ArrayList<Personagem> player;
+        ArrayList<Personagem> playerInimigo;
+        
+        ArrayList<Integer> selecao = new ArrayList();
+        boolean click = true;
+        
+        public void escolhe(int i){
+            if(click){
+                selecao.add(i);
+                click = !click;
+            }
+            else{
+                selecao.add(i);
+                click = !click;
+            }
+            if(selecao.size() >= 2){
+                playerInimigo.get(selecao.get(1)).vida -= player.get(selecao.get(0)).ataque;
+                selecao = new ArrayList();
+            }
+        }
 
-        public TelaGame(ArrayList<Personagem> player){
+        public TelaGame(ArrayList<Personagem> player, ArrayList<Personagem> playerInimigo) {
             this.player = player;
+            this.playerInimigo = playerInimigo;
+            /*for (int k = 0; k < player.size(); k++) {
+                this.add(player.get(k).life);
+            }
+            for (int k = 0; k < playerInimigo.size(); k++) {
+                this.add(playerInimigo.get(k).life);
+            }*/
         }
 
         @Override
         public void paintComponent(Graphics g2) {
-            
             Graphics2D g = (Graphics2D) g2.create();
             g.setColor(Color.BLACK);
             g.fillRect(0, 0, 600, 600);
             g.setStroke(new BasicStroke(3));
-            for(int k = 0; k < player.size(); k++){
-                g.drawImage(player.get(k).playerImagem.get(player.get(k).indice).imagem, player.get(k).x, player.get(k).y, null);
-                player.get(k).update();
+            for (int k = 0; k < player.size(); k++) {
+                if (player.get(k).vida > 0) {
+                    g.drawImage(player.get(k).playerImagem.get(player.get(k).indice).imagem, player.get(k).x, player.get(k).y, null);
+                    player.get(k).update();
+                    player.get(k).life.setBounds(80, player.get(k).y, 50, 10);
+                    this.add(player.get(k).life);
+                }
+                else{
+                    this.remove(player.get(k).life);
+                }
             }
-               
+            for (int k = 0; k < playerInimigo.size(); k++) {
+                if (playerInimigo.get(k).vida > 0) {
+                    g.drawImage(playerInimigo.get(k).playerImagem.get(playerInimigo.get(k).indice).imagem, playerInimigo.get(k).x, playerInimigo.get(k).y, null);
+                    playerInimigo.get(k).update();
+                    playerInimigo.get(k).life.setBounds(450, playerInimigo.get(k).y, 50, 10);
+                    this.add(playerInimigo.get(k).life);
+                }
+                else{
+                    this.remove(playerInimigo.get(k).life);
+                }
+            }
 
             repaint();
         }
-        
+
         @Override
         public void mouseClicked(MouseEvent e) {
-            System.out.println(e.getX());
+            //System.out.println(e.getX());
+            //System.out.println(e.getY());
+
+            if (e.getX() >= 5 && e.getX() <= 80
+                    && e.getY() >= 30 && e.getY() <= 200) {
+                escolhe(0);
+                System.out.println("Naruto");
+            }
+            if (e.getX() >= 5 && e.getX() <= 80
+                    && e.getY() >= 210 && e.getY() <= 380) {
+                escolhe(1);
+                System.out.println("Sasuke");
+            }
+            if (e.getX() >= 5 && e.getX() <= 80
+                    && e.getY() >= 390 && e.getY() <= 600) {
+                escolhe(2);
+                System.out.println("Kakashi");
+            }
+
+            if (e.getX() >= 500 && e.getX() <= 600
+                    && e.getY() >= 30 && e.getY() <= 200) {
+                escolhe(0);
+                System.out.println("Nagato");
+            }
+            if (e.getX() >= 500 && e.getX() <= 600
+                    && e.getY() >= 210 && e.getY() <= 380) {
+                escolhe(1);
+                System.out.println("Madara");
+            }
+            if (e.getX() >= 500 && e.getX() <= 600
+                    && e.getY() >= 390 && e.getY() <= 600) {
+                escolhe(2);
+                System.out.println("Hashirama");
+            }
+
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
-            System.out.println(e.getX());
+            //System.out.println(e.getX());
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
-            System.out.println(e.getX());
+            //System.out.println(e.getX());
         }
 
         @Override
         public void mouseEntered(MouseEvent e) {
-            System.out.println(e.getX());
+            //System.out.println(e.getX());
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
-            System.out.println(e.getX());
+            //System.out.println(e.getX());
         }
 
     }
@@ -126,19 +229,83 @@ public class Minimax {
         for (int i = 0; i < 3; i++) {
             Personagem aux = new Personagem();
             aux.id = i;
+            aux.life.setBackground(Color.red);
+            aux.life.setForeground(Color.green);
+            aux.life.setValue(aux.vida);
+            aux.life.setVisible(true);
             playerInimigo.add(aux);
         }
 
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 3; i++) {
             Personagem aux = new Personagem();
             aux.id = i;
+            aux.life.setBackground(Color.red);
+            aux.life.setForeground(Color.green);
+            aux.life.setValue(aux.vida);
+            aux.life.setVisible(true);
             player.add(aux);
         }
-        ArrayList<PlayerImagem> naruto = new ArrayList<PlayerImagem>();
-        naruto.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Naruto\\50_Asset_90.png")),50,50));
-        naruto.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Naruto\\60_Asset_83.png")),50,50));
-        player.get(0).playerImagem = naruto;
 
+        ArrayList<PlayerImagem> naruto = new ArrayList<PlayerImagem>();
+        naruto.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Naruto\\50_Asset_90.png")), 0, 0));
+        naruto.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Naruto\\60_Asset_83.png")), 0, 0));
+        player.get(0).playerImagem = naruto;
+        ArrayList<PlayerImagem> sasuke = new ArrayList<PlayerImagem>();
+        sasuke.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Sasuke\\1_Asset_43.png")), 0, 0));
+        sasuke.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Sasuke\\43_Asset_40.png")), 0, 0));
+        sasuke.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Sasuke\\115_Asset_39.png")), 0, 0));
+        sasuke.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Sasuke\\139_Asset_41.png")), 0, 0));
+        player.get(1).playerImagem = sasuke;
+        player.get(1).y = 190;
+        ArrayList<PlayerImagem> kakashi = new ArrayList<PlayerImagem>();
+        kakashi.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Kakashi\\86_Asset_38.png")), 0, 0));
+        kakashi.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Kakashi\\96_Asset_33.png")), 0, 0));
+        kakashi.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Kakashi\\101_Asset_37.png")), 0, 0));
+        kakashi.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Kakashi\\106_Asset_35.png")), 0, 0));
+        kakashi.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Kakashi\\111_Asset_34.png")), 0, 0));
+        kakashi.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Kakashi\\115_Asset_36.png")), 0, 0));
+        player.get(2).playerImagem = kakashi;
+        player.get(2).y = 380;
+
+        ArrayList<PlayerImagem> nagato = new ArrayList<PlayerImagem>();
+        nagato.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Nagato\\4_Asset_34.png")), 0, 0));
+        nagato.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Nagato\\5_Asset_35.png")), 0, 0));
+        nagato.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Nagato\\21_Asset_41.png")), 0, 0));
+        nagato.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Nagato\\43_Asset_36.png")), 0, 0));
+        nagato.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Nagato\\65_Asset_38.png")), 0, 0));
+        nagato.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Nagato\\75_Asset_40.png")), 0, 0));
+        nagato.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Nagato\\95_Asset_146.png")), 0, 0));
+        nagato.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Nagato\\126_Asset_37.png")), 0, 0));
+        nagato.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Nagato\\146_Asset_39.png")), 0, 0));
+        nagato = Minimax.virar(nagato);
+        playerInimigo.get(0).playerImagem = nagato;
+        playerInimigo.get(0).x = 500;
+
+        ArrayList<PlayerImagem> madara = new ArrayList<PlayerImagem>();
+        madara.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Madara\\Asset_61.png")), 0, 0));
+        madara.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Madara\\Asset_60.png")), 0, 0));
+        madara.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Madara\\Asset_59.png")), 0, 0));
+        madara.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Madara\\Asset_58.png")), 0, 0));
+
+        madara.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Madara\\Asset_57.png")), 0, 0));
+        madara.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Madara\\Asset_58.png")), 0, 0));
+        madara.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Madara\\Asset_59.png")), 0, 0));
+        madara.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Madara\\Asset_60.png")), 0, 0));
+
+        madara = Minimax.virar(madara);
+        playerInimigo.get(1).playerImagem = madara;
+        playerInimigo.get(1).x = 470;
+        playerInimigo.get(1).y = 170;
+
+        ArrayList<PlayerImagem> hashirama = new ArrayList<PlayerImagem>();
+        hashirama.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Hashirama\\Asset_20.png")), 0, 0));
+        hashirama.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Hashirama\\Asset_21.png")), 0, 0));
+        hashirama.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Hashirama\\Asset_22.png")), 0, 0));
+        hashirama.add(new PlayerImagem((BufferedImage) ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\Hashirama\\Asset_23.png")), 0, 0));
+        hashirama = Minimax.virar(hashirama);
+        playerInimigo.get(2).playerImagem = hashirama;
+        playerInimigo.get(2).x = 470;
+        playerInimigo.get(2).y = 380;
 
         JFrame frame = new JFrame("RPG");
         frame.setSize(600, 600);
@@ -147,27 +314,35 @@ public class Minimax {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        TelaGame game = new TelaGame(player);
+        TelaGame game = new TelaGame(player, playerInimigo);
         game.setVisible(true);
         game.player = player;
         frame.add(game);
+        UIManager.put("ProgressBar.foreground", Color.green);
+
         frame.addMouseListener(game);
 
         No no = new No();
         no.player = player;
         no.playerInimigo = playerInimigo;
-        
-        
-        
 
-        
-        
         player.get(0).ataque = 3;
         //player.get(1).ataque = 2;
         player.get(0).vida = 5;
         playerInimigo.get(1).vida = 4;
         playerInimigo.get(0).vida = 2;
         playerInimigo.get(1).ataque = 4;
+        
+        
+        
+        for (Personagem p : player) {
+            p.life.setMaximum(p.vida);
+            p.life.setValue(p.vida);
+        }
+        for (Personagem p : playerInimigo) {
+            p.life.setMaximum(p.vida);
+            p.life.setValue(p.vida);
+        }
 
         //player.get(1).ataque = 1;
         /*System.out.println("Valor: " + minimax(no, true, MIN, MAX));
@@ -177,7 +352,6 @@ public class Minimax {
         for (int i = 0; i < no.filho.size(); i++) {
             System.out.println(i + " | " + no.filho.get(i).valor);
         }*/
-
     }
 
     public static int minimax(No no, boolean jogada, int alpha, int beta) throws CloneNotSupportedException {
